3761d79347639809754fa8a3ea0cb0b5c1b2c5c6
jmurinello <jm.murinello@gmail.com>
2018-11-30 12:01:33 +0900

    Switch hosting from Heroku to Firebase

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: { main: "./src/index.js" },
@@ -18,6 +19,10 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(['dist'], {
+      root: process.cwd(),
+      exclude: ['404.html'],
+    }),
     new HtmlWebpackPlugin({
       inject: false,
       hash: true,
